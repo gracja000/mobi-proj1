c3c16706f5b0242f749a66c9270d7a8297787aad
Mateusz Wąsowski <mwasowski@newterm.pl>
2020-01-12 14:44:08 +0100

    dalej

--- mobi.py
+++ mobi.py
@@ -4,12 +4,13 @@ import scipy.sparse as sp
 
 # Parametry struktury
 N_a = 2*10**18 #[cm**-3] koncenatracja domieszki akceptorowej
+n_i = 1e10
 t_Ox = 1.3*10**-7
 t_Si = 16/2*10**-7
 V_g = 0.8
 
 #stałe fizyczne
-n_i = 1e10
+
 eps = 8.854187817*10**-14
 eps_Si = eps*11.7
 eps_Ox = eps*3.9
@@ -25,13 +26,20 @@ Nx=100 #liczba wezłow siatki
 eps_aim =10**-15
 
 M = sp.diags([1, -2, 1], [-1, 0, 1], shape=(Nx,Nx),dtype=int).toarray()
+M[0,0] =1; M[0,1] =0
+M[Nx-1,Nx-1] =1; M[Nx-1,Nx-2] =0
+
+psi = np.linspace(V_g,V_g,Nx)
+
+print(psi)
 print(M)
 
 #iteracyjna metoda Newtona
 
 steps_c = 0
-while(eps > eps_aim)
-    steps_c +=1
+#while(eps > eps_aim)
+#    steps_c +=1
+#    print(steps_c)
 
 print(steps_c)
 
